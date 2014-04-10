1edb1956289a1da55e22a079b1e8296b85beb26c
Kirill <kirillSuhodolov@gmail.com>
2014-04-10 11:56:38 +0300

    flat-ui

--- config/application.js
+++ config/application.js
@@ -11,7 +11,14 @@
 //= require moment
 //= require bootstrap
 //= require jquery.mask
-//= require_tree ../vendor/assets/javascripts/jquery-ui
+//= require ../vendor/assets/javascripts/jquery-ui/jquery.ui.core
+//= require ../vendor/assets/javascripts/jquery-ui/jquery.ui.widget
+//= require ../vendor/assets/javascripts/jquery-ui/jquery.ui.mouse
+//= require ../vendor/assets/javascripts/jquery-ui/jquery.ui.position
+//= require ../vendor/assets/javascripts/jquery-ui/jquery.ui.draggable
+//= require ../vendor/assets/javascripts/jquery-ui/jquery.ui.droppable
+//= require ../vendor/assets/javascripts/jquery-ui/jquery.ui.sortable
+
 //= require_tree ../vendor/assets/javascripts/flat-ui
 
 window.App = require('app').default.create();
